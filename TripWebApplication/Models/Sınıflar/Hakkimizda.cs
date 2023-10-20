f958a0b80699aa97b34dfd93764d7d8deb9be2cc
Okan Tırpancı <okantirpanci96@gmail.com>
2023-10-20 17:17:09 +0300

    Add project files.

---
+++ TripWebApplication/Models/Sınıflar/Hakkimizda.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TripWebApplication.Models.Sınıflar
+{
+	public class Hakkimizda
+	{
+		[Key]
+		public int ID { get; set; }
+		public string PhotoUrl { get; set; }
+		public string Aciklama { get; set; }
+	}
+}
