40c38a25935dd849de405d01049d277ef0302369
Okan Tırpancı <okantirpanci96@gmail.com>
2023-10-20 17:44:51 +0300

    save

---
+++ TripWebApplication/Models/Sınıflar/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TripWebApplication.Models.Sınıflar
+{
+	public class Context : DbContext
+	{
+		public DbSet<Admin> Admins { get; set; }
+		public DbSet<AdresBlog> AdresBlogs { get; set; }
+		public DbSet<Blog> Blogs { get; set; }
+		public DbSet<Hakkimizda> Hakkimizdas { get; set; }
+		public DbSet<İletisim> Iletisims { get; set; }
+		public DbSet<Yorumlar> Yorumlars { get; set; }
+	}
+}
