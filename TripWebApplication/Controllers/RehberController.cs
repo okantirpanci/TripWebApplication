eee59f8b6e41143c62a71b4cc0c348a316e6f6b9
Okan Tırpancı <okantirpanci96@gmail.com>
2023-10-23 13:09:36 +0300

    save

--- TripWebApplication/Controllers/RehberController.cs
+++ TripWebApplication/Controllers/RehberController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TripWebApplication.Models.Sınıflar;
 
 namespace TripWebApplication.Controllers
 {
     public class RehberController : Controller
     {
         // GET: Rehber
+        Context db = new Context();
+        RehberPage RehberPage = new RehberPage();
         public ActionResult Index()
         {
-            return View();
+            RehberPage.OtelList = db.Otellers.ToList();
+            return View(RehberPage);
         }
-    }
+	}
 }
